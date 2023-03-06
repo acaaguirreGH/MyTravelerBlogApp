dd7268e09f023c42b52de1f5f578ff720785e0f8
Aldo Castillo <aldo.castillo>
2023-03-06 09:35:51 -0600

    Several changes applied needed for the first page.

--- src/app/post-preview/post-preview.component.ts
+++ src/app/post-preview/post-preview.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostModel, postState } from '../Models/post';
 
 @Component({
   selector: 'app-post-preview',
@@ -6,5 +9,78 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-preview.component.css']
 })
 export class PostPreviewComponent {
+  @Input() postData: PostModel;
+  @Input() filterCat: string;
+  @Output() removeEmitter = new EventEmitter<number>();
+  postEditData: PostModel;
+  showActions = false;
+  commentsLength: number;
+  visiblePost = false;
+  @ViewChild('closebutton1', {static: false}) private closeModal: ElementRef;
+  hover: string = 'out';
+  sameTypeCounter: {catName: '', };
+  
+  constructor(private route: ActivatedRoute, private router: Router, public http: HttpClient) { }
+  
+  ngOnInit() {
+    this.showActions = false;
+    if (this.postData.comments === undefined) {
+      this.commentsLength = 0;
+    } else {
+      this.commentsLength = this.postData.comments.length;
+    }
+  
+  }
 
+  filterPosts() {
+    if (this.filterCat === 'All' || this.filterCat === this.postData.category) {
+      
+      return true;
+    }
+    return false;
+  }
+
+  
+  ReceivePost(editedPost: PostModel) {
+    if(editedPost === null) {
+      this.hideModel1();
+      return;
+    }
+    if (1===1) {
+    //Replace current post id with new data
+    if (localStorage.getItem('EditingPost')) {
+      let postEdit = localStorage.getItem('EditingPost');
+      let parsed = JSON.parse(postEdit!);
+      this.postData = parsed;
+    } else {
+      localStorage.setItem('EditingPost', JSON.stringify(editedPost));
+    }
+    this.hideModel1();
+    }
+  }
+
+  removeEmit() {
+    this.postData.state = postState.Deleted;
+    this.removeEmitter.emit(this.postData.id);
+  }
+
+  editEmit() {
+    if(this.postData !== undefined) {
+      localStorage.setItem('EditingPost', JSON.stringify(this.postData));
+    }    
+  }
+
+  mouseOver() {
+    this.hover = 'in';
+    this.showActions = !this.showActions;
+  }
+
+  mouseOut() {
+    this.hover = 'out';
+    this.showActions = !this.showActions;
+  } 
+
+  hideModel1() {
+    this.closeModal.nativeElement.click();
+  }
 }
--- src/app/posts-holder/posts-holder.component.ts
+++ src/app/posts-holder/posts-holder.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostModel } from '../Models/post';
+import { createApi } from 'unsplash-js';
+import { PostService } from '../services/post-service.service';
 
 @Component({
   selector: 'app-posts-holder',
   templateUrl: './posts-holder.component.html',
   styleUrls: ['./posts-holder.component.css']
 })
-export class PostsHolderComponent {
+export class PostsHolderComponent implements OnInit  {
+  categories: ICategory[];
+  currCat: ICategory;
+  selectedValueCategory: string;
+  newPost: PostModel = {id: 0, category : '', title: '', description: ''};
+  postArray: PostModel[];
+  postArrayF: PostModel[];
+  filterCategory = 'All';
+  lastId: number;
+  activeModal = false;
+  @ViewChild('closebutton', {static: false}) private closeModal: ElementRef;
+  unsplashApi = createApi({
+    accessKey: 'JxTV9Lriy4vflMjdekklqBnq-ODL0aZP9omvh8Jg3mE'
 
+       });
+
+  constructor(public postServiceOne: PostService, private router: Router) { }
+  
+  ngOnInit() {
+  this.categories = this.postServiceOne.GetCategories();
+  if (!localStorage.getItem('EditingPost')) {
+      this.postServiceOne.GetPosts().subscribe(postArray => this.postArray = postArray);
+      this.postArrayF = this.postArray;
+      this.postServiceOne.getUrl();       
+  } else {
+    let postsCache = localStorage.getItem('EditingPost');
+    if (postsCache !== undefined) {
+      let r: PostModel[] = JSON.parse(postsCache!);
+      this.postArray = r;
+      this.postArrayF = this.postArray;
+    }
+  }
+  this.lastId = this.postArray.length;
+  this.activeModal = true;
+  
+}
+
+removePost(postId: number) {
+  const postPos = this.postArray.indexOf(this.postArray.find(x => x.id === postId)!);
+  this.postArray.splice(postPos, 1);
+  if(localStorage.getItem('1')) {
+    localStorage.removeItem( '1');
+    localStorage.setItem( '1', JSON.stringify(this.postArray));
+  } else {
+    localStorage.setItem( '1', JSON.stringify(this.postArray));
+  }
+}
+
+setCategory(option: ICategory) {
+  this.currCat = option;
+  if (option !== undefined ) {
+    this.filterCategory = option.value;
+    if(this.filterCategory !== 'All') {
+      this.postArray = this.postArrayF.filter(x => x.category === this.filterCategory);
+    } else if(this.filterCategory === 'All') {
+      this.postArray = this.postArrayF;
+    }
+  }
+}
+
+filterPosts($event: any) {
+  if (this.filterCategory === 'All' || this.filterCategory === $event) {
+    if($event !== undefined && $event !== '' && $event !== 'All') {
+      this.postArray = this.postArray.filter(x => x.category === $event);
+    } else if(this.filterCategory === 'All') {
+      this.postArray = this.postArrayF;
+    }
+  }
+}
+
+SavePost(data: PostModel) {
+  if (data != null) {
+     if (this.postArray !== undefined) {
+        this.lastId = this.lastId + 1;
+        this.postArrayF.push(
+          {id: data.id = this.lastId, title: data.title, description: data.description, category: data.category, state: data.state});
+          this.lastId = this.lastId + 1;
+        this.newPost = new PostModel;
+        this.hideModel();
+        localStorage.setItem( '1', JSON.stringify(this.postArray));
+        
+        if(this.postArray.length === 0 && this.postArrayF.length > 0) {
+          this.postArray = this.postArrayF;
+        }      
+        if(this.currCat !== undefined){
+          this.setCategory(this.currCat);  
+        }else {
+          this.currCat = {key: 'AllKey', value: 'All'};
+          this.setCategory(this.currCat);
+        }
+        this.postServiceOne.getUrl();
+        
+  }
+}
+}
+
+hideModel() {
+this.closeModal.nativeElement.click();
+}
+}
+interface ICategory {
+  key: string;
+  value: string;
 }
