63c7ae0088ab8f56d85890d16fee1b52556faa12
acaaguirreGH <acaaguirre@gmail.com>
2025-08-15 13:11:58 -0600

    Added basic Navigation menu and Testing Page

---
+++ src/app/dev-site/dev-site.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dev-site',
+  templateUrl: './dev-site.component.html',
+  styleUrls: ['./dev-site.component.css']
+})
+export class DevSiteComponent {
+
+}
---
+++ src/app/sidebarmenu/sidebarmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebarmenu',
+  templateUrl: './sidebarmenu.component.html',
+  styleUrls: ['./sidebarmenu.component.css']
+})
+export class SidebarmenuComponent {
+
+}
