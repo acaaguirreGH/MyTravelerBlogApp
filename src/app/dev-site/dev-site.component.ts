542689191694345860c08c951e17f8ebe3bd1834
acaaguirreGH <acaaguirre@gmail.com>
2025-09-03 12:13:39 -0600

    Azure Functions implementation

--- src/app/dev-site/dev-site.component.ts
+++ src/app/dev-site/dev-site.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TestingAZService } from '../services/testing-az.service';
+import {ChangeDetectionStrategy} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-dev-site',
@@ -6,5 +10,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./dev-site.component.css']
 })
 export class DevSiteComponent {
+
+  imgURL: string = ""
+  textRequest: string = ""
+  imgDescription: string = ""
+
+  constructor(public TestSVC: TestingAZService) {
+
+  }
+
+CallAzFN() {
+
+    try {
+      this.TestSVC.TestAzFN().subscribe((response)=> {
+      this.imgURL = response;
+  });
+      
+    } catch (error) {
+      alert(error);
+    }
+}
+
+CallDallE(event: any) {
+const inputElement = document.getElementById("inputPrompt") as HTMLInputElement;
+  const inputValue = inputElement.value; // Get the value from the input field
+
+  if(inputValue){
+    try {
+      this.TestSVC.CallDallE(inputValue).subscribe((response)=>{
+        if(response){
+          //Use any LLM to get a description for the image
+          this.TestSVC.CallCompVisionFunction(response).subscribe((response)=>{
+              this.imgDescription = response;
+          });
+        }
+
+        this.imgURL = response;
+      });
+      
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  }
 
 }
